06a7f23d6ea1fdfb9111880f9d762bf86bc34bf3
Bernedotcom <val13120@hotmail.fr>
2017-11-06 13:34:57 +0100

    Update ingredient form

--- src/pages/add-ingredient/add-ingredient.ts
+++ src/pages/add-ingredient/add-ingredient.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { iIngredient } from '../../models/ingredient';
-
+import { IngredientService } from '../../providers/ingredient-service/ingredient-service';
 /**
  * Generated class for the AddIngredientPage page.
  *
@@ -17,13 +17,31 @@ import { iIngredient } from '../../models/ingredient';
 export class AddIngredientPage {
 
   //Pizza which will be added
-  ingredient: iIngredient;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ingredient = { name: "", weight: "", price: 0 };
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddIngredientPage');
   }
 
+  /**
+  * Add a new ingredient to the APP
+  **/
+  addIngredient(ingredient : { name:string, weight:string, price:number })
+  {
+    this.ingredient = ingredient;
+    console.log(this.ingredient);
+
+    //Adding the new ingredient to App
+    // this.IngredientService.add(this.ingredient).then(data => {
+    // });
+
+    //Display a toast to confirm new pizza created
+    let toast = this.toastCtrl.create({
+      message: 'L\'ingrédient a bien été créé',
+      duration: 3000
+    });
+    toast.present();
+  }
 }
