758365c70e85e41fd0c18e7e270e8a5422787914
Bernedotcom <val13120@hotmail.fr>
2017-11-23 14:26:05 +0100

    update

--- src/pages/ingredient/ingredient.ts
+++ src/pages/ingredient/ingredient.ts
@@ -17,29 +17,31 @@ import { Ingredient } from '../../models/ingredient';
 })
 export class IngredientPage {
 
-  ingredients: [Ingredient];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public IngredientService: IngredientService, public toastCtrl: ToastController) {
+  ingredients: Ingredient[];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public ingredientService: IngredientService, public toastCtrl: ToastController) {
     //For testing, ingredients list
-    this.ingredients = [{name: "Lardons", weight : "100 Gr", price: 10}, {name: "Pomme de terre ", weight : "300 Gr", price: 2}, {name: "Fromage", weight : "50 Gr", price: 1}];
+    // this.ingredients = [{name: "Lardons", weight : "100 Gr", price: 10}, {name: "Pomme de terre ", weight : "300 Gr", price: 2}, {name: "Fromage", weight : "50 Gr", price: 1}];
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad IngredientPage');
+    //Loading ingredients from API
+    this.ingredientService.get().then(data => {
+      this.ingredients = data
+    });
   }
 
  //Remove an ingredient from the App
   delete(ingredient: Ingredient)
   {
     //When API is running
-    // this.IngredientService.delete(ingredient.name).then(data => {
-    //   this.ingredients = data
-    // });
-
-    //Display a toast to confirm the ingredient is deleted
-    let toast = this.toastCtrl.create({
-      message: 'Ingrédient supprimé',
-      duration: 3000
+    this.ingredientService.delete(ingredient._id).then(data => {
+      //Display a toast to confirm the ingredient is deleted
+      let toast = this.toastCtrl.create({
+        message: 'Ingrédient supprimé',
+        duration: 3000
+      });
+      toast.present();
+      this.navCtrl.setRoot(this.navCtrl.getActive().component);
     });
-    toast.present();
   }
 }
